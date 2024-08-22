0863a95cf0e22ab03146f28610fbc553202d8073
nidhimamman3 <nidhimamman3@gmail.com>
2024-08-22 14:33:12 +0530

    commit

--- User/src/Pages/Signin.jsx
+++ User/src/Pages/Signin.jsx
@@ -30,15 +30,14 @@ const Signin = () => {
       const response = await axios.post(`${BASE_URL}/api/signin`, formData);
       const res_data = response.data;
 
-      console.log(res_data)
 
       storeTokenInLS(res_data.token);
 
       // Check for admin status
       if (response.status === 200) {
+    
 
-
-        toast(`Welcome to UMS, ${res_data.firstName || 'User'}!`);
+        toast('Signed in successfully');
 
         // Navigate based on user role
         if (res_data.isAdmin) {
