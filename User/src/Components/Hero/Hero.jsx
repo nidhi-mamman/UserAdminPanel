8a87c9a47009edfadaf02b38a0c99f5242845a32
nidhimamman3 <nidhimamman3@gmail.com>
2024-08-22 13:24:16 +0530

    minor commit

--- User/src/Components/Hero/Hero.jsx
+++ User/src/Components/Hero/Hero.jsx
@@ -1,13 +1,13 @@
 /* eslint-disable react/no-unescaped-entities */
-// import "./Hero.css";
+import "./Hero.css";
 import hero from "../../assets/webdev.png";
-// import { Link } from "react-router-dom";
-// import { HiLightBulb } from "react-icons/hi";
+import { Link } from "react-router-dom";
+import { HiLightBulb } from "react-icons/hi";
 import { useAuth } from "../../Context/AuthContext";
 
 const Hero = () => {
 
-  const {user}=useAuth()
+  const { user } = useAuth()
 
   return (
     <>
@@ -15,46 +15,29 @@ const Hero = () => {
         <div className="hero-img ">
           <img src={hero} alt="" />
         </div>
-        <div className="hero-content ">
-          <h1 className="font-bold text-5xl mb-10 text-purple-700 ">
+        <div className="hero-content">
+          <h1>
             Hello! <span>{user.firstName}</span>
           </h1>
-          <p className="text-white  text-2xl p-2 text-justify">
+          <p >
             User Management System includes a admin and user,the admin is able
             to control all the CRUD operations of the backend that will only be
             shown to admin when he logs in.While the user can also register
             itself but it cannot see the backend data.If the admin logins in he
             will be able to see all the users who have logged in.while the user
             can only send message to the admin.
+            <div className="know-more">
+              <Link to="/about">
+                <div className="icon">
+                  <HiLightBulb color="white" size={25} className="bulb" />
+                  <button className="know">
+                    Know More
+                  </button>
+                </div>
+              </Link>
+            </div>
           </p>
         </div>
-        {/* <div className="hero-img">
-          <img src={hero} alt=""/>
-        </div>
-        <div className="hero-content">
-          <h1 className="font-bold text-4xl mb-10 text-purple-700">
-            Hi,<span className="text-4xl text-white">{user.firstName}</span> to User management System
-          </h1>
-          <p className="text-white">
-            User Management System includes a admin and user,the admin is able
-            to control all the CRUD operations of the backend that will only be
-            shown to admin when he logs in.While the user can also register
-            itself but it cannot see the backend data.If the admin logins in he
-            will be able to see all the users who have logged in.while the user
-            can only send message to the admin.
-          </p>
-          <div className="know-more relative">
-            <Link to="/about">
-              <div className="icon flex items-center">
-                <HiLightBulb color="white" size={25} className="bg-purple-700 absolute left-7"/>
-                <button className="text-white know bg-purple-700 text-center">
-                  Know More
-                </button>
-              </div>
-            </Link>
-          </div>
-        </div> */}
-
       </div>
     </>
   );
