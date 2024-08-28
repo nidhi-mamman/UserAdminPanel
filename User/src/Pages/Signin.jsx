ecd44bce1543e0df666313ae817952db394a34e0
Nidhi <109498013+nidhi-mamman@users.noreply.github.com>
2024-08-28 20:54:16 +0530

    Update Signin.jsx

--- User/src/Pages/Signin.jsx
+++ User/src/Pages/Signin.jsx
@@ -37,7 +37,7 @@ const Signin = () => {
     
 
         toast('Signed in successfully');
-        toast(`Welcome to UMS, ${user?.email|| 'User'}!`);
+        toast(`Welcome to UMS, ${res_data.email|| 'User'}!`);
 
         // Navigate based on user role
        setTimeout(() => {
